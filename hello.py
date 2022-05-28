9807583a2cf7c0c001473bd0f016ea4ba3b4a397
Saranesh Maniratna Kanumuri <smkanumuri@kagool.com>
2022-05-28 23:41:40 +0530

    read some data from csv

--- hello.py
+++ hello.py
@@ -1,3 +1,3 @@
 import pandas as pd
-df = pd.read_csv("git-github/Business_Line_Override_Leysap.csv")
+df = pd.read_csv("C:/Users/smkanumuri/Desktop/kagool/LearningandDev/GIT/git-github/Business_Line_Override_Leysap.csv")
 df.head()
